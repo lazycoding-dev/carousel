ae4d0a4ed297a2b933bb47e3bc0093253758fa57
LazyCoding <blog.lazycoding.dev@gmail.com>
2021-01-13 22:19:23 +0100

    chore: sync with post

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Carousel } from './Carousel'
 
 function App() {
+  const images = [
+    'https://images.pexels.com/photos/374006/pexels-photo-374006.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=600&w=800',
+    'https://images.pexels.com/photos/374897/pexels-photo-374897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=600&w=800',
+    'https://images.pexels.com/photos/301703/pexels-photo-301703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=600&w=800',
+  ]
   return (
     <div style={{ margin: '100px 0', height: '70vh', width: '100%' }}>
-      <Carousel images={['img1.jpg', 'img2.jpg', 'img3.jpg']} />
+      <Carousel images={images} />
     </div>
   )
 }
